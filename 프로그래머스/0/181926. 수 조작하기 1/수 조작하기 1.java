0dced2d5256bab7c51ead88cfd2d5b9933e11c1b
Sohee Park <seobeusohui@gmail.com>
2026-03-18 11:32:54 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.20 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for (int i = 0; i < control.length(); i++) {
+            char temp = control.charAt(i);
+            
+            switch (temp) {
+                case 'w' : answer += 1; break;
+                case 's' : answer -= 1; break;
+                case 'd' : answer += 10; break;
+                case 'a' : answer -= 10; break;
+            }
+        }
+        return answer;
+    }
+}
