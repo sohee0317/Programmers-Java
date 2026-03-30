92bb39269db8efa9c2a1ab2178ed2540c824ac1a
Sohee Park <seobeusohui@gmail.com>
2026-03-30 16:20:47 +0900

    [level 0] Title: 홀짝 구분하기, Time: 267.93 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -5,12 +5,6 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        if (n % 2 == 0) {
-            // 짝수
-            System.out.printf("%d is even\n", n);
-        } else {
-            // 홀수
-            System.out.printf("%d is odd\n", n);
-        }
+        System.out.printf("%d is %s%n", n, n % 2 == 0 ? "even" : "odd");
     }
 }
