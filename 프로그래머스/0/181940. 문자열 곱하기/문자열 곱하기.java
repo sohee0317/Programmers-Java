9ac63f20682ad72967cb9b0b3aa2034f923fbc1d
Sohee Park <seobeusohui@gmail.com>
2026-03-31 13:19:32 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.09 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String my_string, int k) {
-        String answer = "";
-        
-        for (int i  = 0; i < k; i++) {
-            answer += my_string;
-        }
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < k; i++) answer.append(my_string);
+        return answer.toString();
     }
 }
