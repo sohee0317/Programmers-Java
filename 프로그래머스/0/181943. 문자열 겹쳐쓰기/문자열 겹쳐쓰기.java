e5a3f96414a5e254aad2f030d160bf27ce43fbbc
Sohee Park <seobeusohui@gmail.com>
2026-03-31 10:19:04 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.06 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        return my_string.substring(0, s)
-            + overwrite_string
-            + my_string.substring(s + overwrite_string.length());
+        StringBuilder answer = new StringBuilder();
+        answer.append(my_string.substring(0, s)).append(overwrite_string).append(my_string.substring(s + overwrite_string.length()));
+        return answer.toString();
     }
 }
