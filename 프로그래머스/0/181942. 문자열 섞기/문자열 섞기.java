f253f708cfa7cecc95d634b2e0100b95599037a2
Sohee Park <seobeusohui@gmail.com>
2026-03-31 10:44:36 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,12 +1,8 @@
 class Solution {
     public String solution(String str1, String str2) {
-        int len = str1.length();
-        char[] chars = new char[len * 2];
+        StringBuilder answer = new StringBuilder();
         
-        for (int i = 0; i < len; i++) {
-            chars[i * 2] = str1.charAt(i);
-            chars[i * 2 + 1] = str2.charAt(i);
-        }
-        return new String(chars);
+        for (int i = 0; i < str1.length(); i++) answer.append(str1.charAt(i)).append(str2.charAt(i));
+        return answer.toString();
     }
 }
