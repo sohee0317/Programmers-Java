eb16d76b67bfc667ebf2622dd8844b74fd32ab35
Sohee Park <seobeusohui@gmail.com>
2026-04-01 11:07:37 +0900

    [level 0] Title: 문자열 출력하기, Time: 165.13 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,6 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        System.out.print(a);
+        System.out.println(a);
     }
 }
